37baf051eacde5262ad35d77a20c43b43eca2e67
Eric <heimleiter.dasheim@gmail.com>
2023-02-26 00:00:38 +0100

    camera functionality

--- Assets/Scripts/InputManaging/CameraController.cs
+++ Assets/Scripts/InputManaging/CameraController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Update is called once per frame
-    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private float movementSpeed = 10f, maxZoom = 50f, minZoom = 100f, zoomSpeed = 10f;
     public bool moveWithMouse { get; set; } = true;
+    private Camera _camera;
+    private Vector3 dragOrigin;
+    private void Awake() {
+        _camera = GetComponent<Camera>();
+    }
     void Update()
     {
         if(Input.GetKey(KeyCode.UpArrow)) {
@@ -18,5 +23,24 @@ public class CameraController : MonoBehaviour
         } else if(Input.GetKey(KeyCode.RightArrow)) {
             transform.position += transform.right * movementSpeed * Time.deltaTime;
         }
+        ;
+        if(moveWithMouse == true) {
+            if(Input.GetMouseButtonDown(0)) {
+                dragOrigin = _camera.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if(Input.GetMouseButton(0)) {
+                Vector3 dir = dragOrigin - _camera.ScreenToWorldPoint(Input.mousePosition);
+                transform.position += dir;
+            }
+        }
+        
+        if(Input.GetAxis("Mouse ScrollWheel") != 0f) {
+            Zoom(Input.GetAxis("Mouse ScrollWheel"));
+        }
+    }
+
+    public void Zoom(float intensity) {
+        _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize - (intensity * zoomSpeed), minZoom, maxZoom);
     }
 }
