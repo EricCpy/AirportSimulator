c5e973846af2b43cbb1e09952d1f590a8aba9268
Eric <heimleiter.dasheim@gmail.com>
2023-04-07 16:42:43 +0200

    added ruleengine package

--- Assets/Scripts/InputManaging/CameraController.cs
+++ Assets/Scripts/InputManaging/CameraController.cs
@@ -10,39 +10,63 @@ public class CameraController : MonoBehaviour
     public bool moveWithMouse { get; set; } = true;
     private Camera _camera;
     private Vector3 dragOrigin;
-    private void Awake() {
+    private float CellSize { get => BuildingSystem.Instance.grid.GetCellSize(); }
+    private float width, height;
+    private void Awake()
+    {
         _camera = GetComponent<Camera>();
+    }
+    private void Start()
+    {
+        width = BuildingSystem.Instance.grid.GetWidth() * CellSize;
+        height = BuildingSystem.Instance.grid.GetHeight() * CellSize;
+        transform.position = new Vector3(width / 2, height / 2, -10);
     }
     void Update()
     {
-        if(GameManager.Instance.uiOpen) return;
-        if(Input.GetKey(KeyCode.UpArrow)) {
+        if (GameManager.Instance.uiOpen) return;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
             transform.position += transform.up * movementSpeed * Time.unscaledDeltaTime;
-        } else if(Input.GetKey(KeyCode.DownArrow)) {
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
             transform.position += -transform.up * movementSpeed * Time.unscaledDeltaTime;
-        } else if(Input.GetKey(KeyCode.LeftArrow)) {
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
             transform.position += -transform.right * movementSpeed * Time.unscaledDeltaTime;
-        } else if(Input.GetKey(KeyCode.RightArrow)) {
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.position += transform.right * movementSpeed * Time.unscaledDeltaTime;
         }
 
-        if(moveWithMouse) {
-            if(Input.GetMouseButtonDown(0)) {
+        if (moveWithMouse)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
                 dragOrigin = _camera.ScreenToWorldPoint(Input.mousePosition);
             }
 
-            if(Input.GetMouseButton(0)) {
+            if (Input.GetMouseButton(0))
+            {
                 Vector3 dir = dragOrigin - _camera.ScreenToWorldPoint(Input.mousePosition);
                 transform.position += dir;
             }
         }
-        
-        if(Input.GetAxis("Mouse ScrollWheel") != 0f) {
+        transform.position = new Vector3(Mathf.Min(Mathf.Max(transform.position.x, 0), width),
+                                         Mathf.Min(Mathf.Max(transform.position.y, 0), height),
+                                         transform.position.z);
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
             Zoom(Input.GetAxis("Mouse ScrollWheel"));
         }
     }
 
-    public void Zoom(float intensity) {
+    public void Zoom(float intensity)
+    {
         _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize - (intensity * zoomSpeed), minZoom, maxZoom);
     }
 }
